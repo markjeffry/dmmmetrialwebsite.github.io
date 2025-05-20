7972276eae4a574116579810225d67feaa0d645c
Jepoy De Leon <84220729+markjeffry@users.noreply.github.com>
2025-05-20 21:52:19 +0800

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Department of Mining, Metallurgical and Materials Engineering &#8212; WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='stylesheet' id='dashicons-css' href='https://dmmme.coe.upd.edu.ph/wp-includes/css/dashicons.min.css?ver=6.4.4' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://dmmme.coe.upd.edu.ph/wp-includes/css/buttons.min.css?ver=6.4.4' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://dmmme.coe.upd.edu.ph/wp-admin/css/forms.min.css?ver=6.4.4' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://dmmme.coe.upd.edu.ph/wp-admin/css/l10n.min.css?ver=6.4.4' media='all' />
+<link rel='stylesheet' id='login-css' href='https://dmmme.coe.upd.edu.ph/wp-admin/css/login.min.css?ver=6.4.4' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://dmmme.coe.upd.edu.ph/wp-content/uploads/2021/06/cropped-logo_dmmme-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://dmmme.coe.upd.edu.ph/wp-content/uploads/2021/06/cropped-logo_dmmme-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://dmmme.coe.upd.edu.ph/wp-content/uploads/2021/06/cropped-logo_dmmme-180x180.png" />
+<meta name="msapplication-TileImage" content="https://dmmme.coe.upd.edu.ph/wp-content/uploads/2021/06/cropped-logo_dmmme-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://dmmme.coe.upd.edu.ph/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://dmmme.coe.upd.edu.ph/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://dmmme.coe.upd.edu.ph/password-reset/">Lost your password?</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="https://dmmme.coe.upd.edu.ph/">&larr; Go to Department of Mining, Metallurgical and Materials Engineering</a>		</p>
+			</div>
+			<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"https:\/\/dmmme.coe.upd.edu.ph\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0" id="regenerator-runtime-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/dist/hooks.min.js?ver=c6aec9a8d4e5a5d543a1" id="wp-hooks-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/dist/i18n.min.js?ver=7701b0c3857f914212ef" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-admin/js/password-strength-meter.min.js?ver=6.4.4" id="password-strength-meter-js"></script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-includes/js/wp-util.min.js?ver=6.4.4" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"7e9ec50ca0"};
+</script>
+<script src="https://dmmme.coe.upd.edu.ph/wp-admin/js/user-profile.min.js?ver=6.4.4" id="user-profile-js"></script>
+	</body>
+	</html>
+	
